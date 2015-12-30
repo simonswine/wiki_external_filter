90d6e68e615a4e2504852479ccace48f08623a70
Christian Simon <simon@swine.de>
2015-12-30 14:04:12 +0100

    Specify method GET for routes
    * compatibility with Redmine 3.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 if Rails::VERSION::MAJOR >= 3
   Rails.application.routes.draw do 
-    match 'wiki_external_filter', :to => 'wiki_external_filter#filter'
-    match 'wiki_external_filter/:filename', :controller => 'wiki_external_filter', :action => 'filter', :macro => 'video', :index => '1', :requirements => { :filename => /video\.flv/ }
+    match 'wiki_external_filter', :to => 'wiki_external_filter#filter', :via => [:get]
+    match 'wiki_external_filter/:filename', :controller => 'wiki_external_filter', :via => [:get], :action => 'filter', :macro => 'video', :index => '1', :requirements => { :filename => /video\.flv/ }
 
-    match 'wiki_external_filter/:filename', :controller => 'wiki_external_filter', :action => 'filter', :macro => 'video_url', :index => '1', :requirements => { :filename => /video_url\.flv/ }
+    match 'wiki_external_filter/:filename', :controller => 'wiki_external_filter', :via => [:get], :action => 'filter', :macro => 'video_url', :index => '1', :requirements => { :filename => /video_url\.flv/ }
   end
 else
   ActionController::Routing::Routes.draw do |map|
